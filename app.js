95867b65f410e46d7ea8b7e2a3489ecc8aadc24c
lily6427 <a2298368256@163.com>
2019-06-23 11:42:13 +0800

    首页布局 注册50%

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 // 设置 默认采用的模板引擎名称
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 // 设置模板页面的存放路径
-app.set('views', './views')
+app.set('views', './views');
 
-app.use('/node_modules', express.static('node_modules'))
+app.use('/node_modules', express.static('node_modules'));
 
 
 app.get('/', (req, res) => {
-  res.render('index', {})
-})
+    res.render('index', {});
+});
 
+
+app.get('/register', (req, res) => {
+    res.render('./user/register.ejs', {});
+});
+
+app.get('/login', (req, res) => {
+    res.render('./user/login.ejs', {});
+});
+
+app.post('/register', (req, res) => {
+
+    res.send({ status: "ok", msg: "注册成功" });
+
+});
 
 app.listen(3000, () => {
-  console.log("服务器运行成功……")
-})
+    console.log("服务器运行成功……");
+});
